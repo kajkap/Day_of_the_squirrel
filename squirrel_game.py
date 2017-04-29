75d9972fe9d33442bce6ad346eaac30905f956c0
Maciej Nowak <mcnowak@wp.pl>
2017-04-29 09:48:40 +0200

    Added comments, docstrings, code cleaned.

--- squirrel_game.py
+++ squirrel_game.py
@@ -1,27 +1,76 @@
 import os
+import time
 
 
-def create_board(width, height):
+def create_board(columns, lines):
+    """Function creates list representing our gameboard.
+
+    Args:
+        columns (int): number of gameboard columns
+        lines (int): number of gameboard rows
+
+    Return:
+        board (list): list of board rows (list)
+    """
+
     board = []
-    for row in range(height):
-        board.append(['X'] * width)
-    for column in range(height)[1:-1]:
-        for i in range(width)[1:-1]:
-            board[column][i] = ' '
+    full_line = ['X' for i in range(columns)]   # creates full row of the board
+    medium_line = [' ' for i in range(columns-2)]   # creates space inside medium rows
+    for i in range(lines):  # creates board as list of lists (rows)
+        board.append(full_line.copy())
+        if i > 0 and i < lines - 1:
+            board[i][1:(columns-1)] = medium_line
     return board
 
 
 def print_board(board):
+    """Function prints list representing our gameboard.
+
+    Args:
+        board (list): list of board rows (list)
+    """
+
     for row in board:
         print(''.join(row))
 
 
 def insert_player(board, x_player, y_player):
+    """Function inserts player character into the list representing our gameboard.
+
+    Args:
+        board (list): list of board rows (list)
+
+    Return:
+        board (list): list of board rows (list) after player insertion
+    """
+
     board[y_player][x_player] = '@'
     return board
 
 
+def clear_player(board, x_player, y_player):
+    """Function clears the place occupied by the player character in the list representing our gameboard.
+
+    Args:
+        board (list): list of board rows (list)
+        x_player (int): horizontal position of player on the board
+        y_player (int): vertical position of player on the board
+
+    Return:
+        board (list): list of board rows (list) after clearing the player position
+    """
+
+    board[y_player][x_player] = ' '
+    return board
+
+
 def getch():
+    """Function returns the button pressed by user.
+
+    Return:
+        ch (str): button pressed by user
+    """
+
     import sys, tty, termios
     fd = sys.stdin.fileno()
     old_settings = termios.tcgetattr(fd)
@@ -33,31 +82,52 @@ def getch():
     return ch
 
 
-def user_control(board, x_player, y_player):
-    button_pressed = getch()
-    if button_pressed == 'd' and board[y_player][x_player + 1] != 'X':
+def user_control(board, x_player, y_player, button_pressed):
+    """Function controls user position on the board based on the button pressed by user.
+        Movent towards the obstacle ('X') is forbidden.
+
+    Args:
+        board (list): list of board rows (list)
+        x_player (int): horizontal position of player on the board
+        y_player (int): vertical position of player on the board
+        button_pressed (str): button pressed by user
+
+    Return:
+        x_player (int): horizontal position of player on the board
+        y_player (int): vertical position of player on the board
+    """
+
+    place_on_right_side = board[y_player][x_player + 1]
+    place_on_left_side = board[y_player][x_player - 1]
+    place_on_up_side = board[y_player - 1][x_player]
+    place_on_down_side = board[y_player + 1][x_player]
+
+    if button_pressed == 'd' and place_on_right_side != 'X':
         x_player += 1
-    elif button_pressed == 'a' and board[y_player][x_player - 1] != 'X':
+    elif button_pressed == 'a' and place_on_left_side != 'X':
         x_player -= 1
-    elif button_pressed == 'w' and board[y_player - 1][x_player] != 'X':
+    elif button_pressed == 'w' and place_on_up_side != 'X':
         y_player -= 1
-    elif button_pressed == 's' and board[y_player + 1][x_player] != 'X':
+    elif button_pressed == 's' and place_on_down_side != 'X':
         y_player += 1
-    elif button_pressed == '\\':
-        x_player = 'quit'
     return x_player, y_player
 
 
 def main():
     button_pressed = ''
-    x_player = 1
-    y_player = 1
-    while x_player != 'quit':
-        os.system('clear')
-        board = create_board(120, 40)
-        board = insert_player(board, x_player, y_player)
-        print_board(board)
-        x_player, y_player = user_control(board, x_player, y_player)
+    x_player = 1    # player's initial horizontal position
+    y_player = 1    # player's initial vertical position
+    board = create_board(120, 40)   # creation of the gameboard
+
+    while button_pressed != '\\':
+        os.system('clear')  # clears terminal screen
+        board = insert_player(board, x_player, y_player)    # inserts player character on the gameboard
+        print_board(board)  # displays gameboard
+        board = clear_player(board, x_player, y_player)  # clears place on the gameboard occupied by user
+        button_pressed = getch()    # reads button pressed by user
+        # changes user position based on pressed button
+        x_player, y_player = user_control(board, x_player, y_player, button_pressed)
 
 
-main()
+if __name__ == '__main__':
+    main()
